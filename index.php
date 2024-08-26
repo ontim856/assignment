9966f54936c821497eb01bfb1191de34f05bc1ed
ontim856 <ontim200166@gmail.com>
2024-08-26 09:43:25 +0600

    change code in index file

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Assignment</title>
+</head>
+<body>
+    <?php
+    echo "akash";
+    ?>
+</body>
+</html>
